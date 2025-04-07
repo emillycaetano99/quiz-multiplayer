67c4e584c8c2bfc92fce1776ecbf398fa42b14c1
emillycaetano99 <emillycaetano99@hotmail.com>
2025-04-07 18:21:27 -0300

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', padding: '2rem' }}>
+      <h1>Quiz Multiplayer</h1>
+      <p>Em breve: Jogo de perguntas e respostas entre dois jogadores!</p>
+    </div>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
